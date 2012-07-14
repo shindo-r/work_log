a1f56eacca04dc9f610525fe61dae0f43ff196d5
shindoryohei <m5111123@gmail.com>
2012-07-14 14:02:17 +0900

    modify test code for new file/dir structure

--- test/model/test_work_log.js
+++ test/model/test_work_log.js
@@ -1,30 +1,31 @@
 module("WorkLog", {
-	setup: function() {
-		work_log = new WorkLog({task:"task_name", hour:"1.0"})
-	}
+  setup: function() {
+    work_log = new WorkLog({task:"task_name", hour:"1.0"})
+  }
 });
 
 test("invalid when 'task' is blank", function(){
-	work_log.task = ""
-	equal(work_log.isValid(), false, "isValid() == false")
+  work_log.task = ""
+  equal(work_log.isValid(), false, "isValid() == false")
+  equal(work_log.isValid(), true, "hoge")
 });
 
 test("valid when 'task' is not blank", function(){
-	work_log.task = "aaa"
-	equal(work_log.isValid(), true, "isValid() == true")
+  work_log.task = "aaa"
+  equal(work_log.isValid(), true, "isValid() == true")
 });
 
 test("invalid when 'hour' is blank", function(){
-	work_log.hour = ""
-	equal(work_log.isValid(), false, "isValid() == false")
+  work_log.hour = ""
+  equal(work_log.isValid(), false, "isValid() == false")
 });
 
 test("invalid when 'hour' is not number", function(){
-	work_log.hour = "aaaa"
-	equal(work_log.isValid(), false, "isValid() == false")
+  work_log.hour = "aaaa"
+  equal(work_log.isValid(), false, "isValid() == false")
 });
 
 test("valid when 'hour' is not blank and 'hour' is number", function(){
-	work_log.hour = "1.0"
-	equal(work_log.isValid(), true, "isValid() == true")
+  work_log.hour = "1.0"
+  equal(work_log.isValid(), true, "isValid() == true")
 });
